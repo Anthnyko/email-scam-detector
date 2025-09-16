ad6b796321a84522c885f204be91992641b0c952
Anthony Co <anthnyco77@gmail.com>
2025-09-16 10:26:31 -0700

    Initial commit: MVP scam detector

---
+++ src/main.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import nltk
+from sklearn.feature_extraction.text import TfidfVectorizer
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import classification_report
+from sklearn.metrics import mean_absolute_error
+
+
+# Imported, changed column names, and changed labels to 1s and 0s
+df = pd.read_csv("data\spam.csv", encoding='latin-1')[['v1', 'v2']]
+df.columns = ['label', 'text']
+df['label'] = df['label'].map({'ham' : 0, 'spam' : 1})
+
+
+# Using NLTK to tokenize and clean out common words
+vectorizer = TfidfVectorizer(stop_words=stopwords.words('english'))
+X = vectorizer.fit_transform(df['text'])
+y = df['label']
+
+
+# Training the model
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
+model = MultinomialNB()
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+print(classification_report(y_test, y_pred))
+print(mean_absolute_error(y_test, y_pred))
+
+
+# Function to classify new email texts
+def classify_email(text):
+    vec = vectorizer.transform([text])
+    prediction = model.predict(vec)[0]
+    return "Scam" if prediction == 1 else "Safe"
+
+print(classify_email("Congratulations! You've won a free cruise. Click here to claim."))
